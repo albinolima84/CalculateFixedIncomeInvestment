3360a8f03cf872967e528099b443c259068ea9b1
Albino Pereira Lima <albino.lima@easynvest.com.br>
2020-10-30 18:05:24 -0300

    Adiciona swagger e cria validações nas controllers

--- api/controllers/import-data.js
+++ api/controllers/import-data.js
@@ -1,6 +1,30 @@
 const importService = require('../services/import-cdi');
+const repository = require('../repositories/cdi-repository');
 
 exports.post = async (request, response) => {
-    await importService.sync();
-    response.status(200).json('Dados importados com sucesso');
+    try{
+        await importService.sync();
+        response.status(200).json({code: 0, message: 'Data imported successfully'});
+    }
+    catch(error){
+        console.log(error);
+        response.status(500).json({code: 500, message: 'unexpected error'});
+    }
+};
+
+exports.get = async (req, res, next) => {
+    try{
+        var rates = await repository.getAll();
+        if (rates.length > 0) {
+            res.status(200).send(rates);
+        }
+        else {
+            res.status(204).send();
+        }
+        
+    }
+    catch(error){
+        console.log(error);
+        response.status(500).json({code: 500, message: 'unexpected error'});
+    }
 };
--- api/routes/calculator-route.js
+++ api/routes/calculator-route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const calculatorController = require('../controllers/calculator');
 
-router.get('/', calculatorController.get);
 router.post('/', calculatorController.post);
 
 module.exports = router;
