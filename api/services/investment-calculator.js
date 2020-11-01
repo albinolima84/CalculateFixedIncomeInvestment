8cdd836cdcae66afb05005458fca570aa8cefdbc
Albino Pereira Lima <albino.lima@easynvest.com.br>
2020-11-01 20:24:57 -0300

    Adiciona valor do investimento no retorno, por dia

--- api/services/investment-calculator.js
+++ api/services/investment-calculator.js
@@ -1,6 +1,6 @@
 const repository = require('../repositories/cdi-repository');
 const businessDayPerYar = 252;
-const rateBusinessPerYear = 1 / businessDayPerYar;
+const rateBusinessDayPerYear = 1 / businessDayPerYar;
 
 getRatesInterval = async(investmentDate, currentDate) => {
     let rates = await repository.getRates(investmentDate, currentDate);
@@ -8,17 +8,18 @@ getRatesInterval = async(investmentDate, currentDate) => {
     return rates;
 };
 
-exports.Calculate = async(investmentDate, currentDate, cdbRate) => {
+exports.Calculate = async(investmentDate, currentDate, cdbRate, investmentValue) => {
     let rates = await getRatesInterval(investmentDate, currentDate);
     let ratesInPeriod = [];
     let tdci_accumulated = 1;
 
     rates.forEach(element => {
         let cdi = element.rateValue;
-        let tcdi = parseFloat((Math.pow(1 + (cdi / 100), rateBusinessPerYear) - 1).toFixed(8));
+        let tcdi = parseFloat((Math.pow(1 + (cdi / 100), rateBusinessDayPerYear) - 1).toFixed(8));
 
         tdci_accumulated += parseFloat((tcdi * cdbRate / 100).toFixed(16));
-        ratesInPeriod.push({date: element.rateDate, tcdi, tdci_accumulated});
+        let value = investmentValue * tdci_accumulated;
+        ratesInPeriod.push({date: element.rateDate, tcdi, tdci_accumulated, value});
     });
     
     return ratesInPeriod;
